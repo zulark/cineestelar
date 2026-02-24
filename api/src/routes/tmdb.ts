fa99d5415479d2efa21014159c793bca186aee3a
Felipe Rudnik <est.luiz.felipe@celepar.pr.gov.br>
2026-02-24 11:06:06 -0300

    removido console log

--- api/src/routes/tmdb.ts
+++ api/src/routes/tmdb.ts
@@ -3,8 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log("Token carregado:", process.env.TMDB_TOKEN ? "Sim" : "Não");
-
 const tmdb = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
     headers: {
